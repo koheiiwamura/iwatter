88d5ede14d00d336705c26a673d36862d9815347
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2017-02-09 04:44:44 +0900

    set user show

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  def show
+    # @tweets = Tweet.where(user_id:params[:id])
+    @user = User.find(params[:id])
+    @tweets = Tweet.page(params[:page]).where(user_id:params[:id]).per(10).order("created_at DESC")
+  end
+end
