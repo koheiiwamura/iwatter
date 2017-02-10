0f624bd40190e32df5347f8d215fbbea8f52bd4b
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2017-02-10 18:45:06 +0900

    fix view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,18 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def show
-    # @tweets = Tweet.where(user_id:params[:id])
     @user = User.find(params[:id])
     @tweets = Tweet.page(params[:page]).where(user_id:params[:id]).per(10).order("created_at DESC")
   end
 
   def following
+    @user = User.find(params[:id])
+    @users = @user.followed
   end
 
   def followers
+    @user = User.find(params[:id])
+    @users = @user.followers
   end
 
 end
