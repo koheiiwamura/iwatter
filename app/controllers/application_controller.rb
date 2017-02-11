155f625a3938b42a016a6713b71de245616526b9
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2017-02-11 15:08:22 +0900

    fix signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :username, :visibility])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :username, :email, :visibility])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:nickname, :password])
   end
 
