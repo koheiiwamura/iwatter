411dc5922c32f829c4d80563877d848a2640c0e4
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2017-02-13 13:22:26 +0900

    js for follow and unfollow

---
+++ app/assets/javascripts/follow.js
@@ -0,0 +1,16 @@
+$(function(){
+  $(".js-follow").on('click', function(){
+      if(confirm('本当にフォローしますか？')){
+      }
+      else{
+          return false;
+      }
+  });
+  $(".js-unfollow").on('click', function(){
+      if(confirm('本当にフォローをやめますか？')){
+      }
+      else{
+          return false;
+      }
+  });
+});
