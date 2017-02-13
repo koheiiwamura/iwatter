88b3a60e8a19dfa75156ceef0ed635811bdfd94b
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2017-02-13 10:01:28 +0900

    create method of follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,4 +32,16 @@ class User < ApplicationRecord
   def email_changed?
     false
   end
+
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    followed.include?(other_user)
+  end
 end
