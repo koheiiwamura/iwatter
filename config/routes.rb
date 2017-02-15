8e522c955932bdda60e23c5f1f3a22ba2e5db35d
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2017-02-15 15:38:59 +0900

    routes of likes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "tweets#index"
-  resources :tweets, only: [:index, :create, :destroy]
+  resources :tweets, only: [:index, :create, :destroy] do
+    resources :likes, only: [:create, :destroy]
+  end
   get 'users/search' => 'users/search#index'
   get 'users/search/find' => 'users/search#find'
   resources :users, only: :show do
     member do
       get :following, :followers
     end
+    resources :likes, only: :index
   end
   resources :relationships, only: [:create, :destroy]
 end
